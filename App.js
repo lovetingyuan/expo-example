81ffda186e7cc9da6147fc9c9448b845ef111525
胡浩 <huhao@seeyon.com>
2023-05-12 11:00:24 +0800

    chore

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! this is change test github action</Text>
+      <Text>this is dev branch</Text>
       <StatusBar style="auto" />
     </View>
   )
