1d2306e4e3aa5fdee99db1f8cca5b75088eceeb5
胡浩 <huhao@seeyon.com>
2023-05-12 11:29:39 +0800

    chore

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import * as Application from 'expo-application'
+import React from 'react'
 
 export default function App() {
+  const [updateTime, setUpdateTime] = React.useState('')
+  React.useEffect(() => {
+    Application.getLastUpdateTimeAsync().then(time => {
+      setUpdateTime(time)
+    })
+  }, [])
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! this is change test github action</Text>
       <Text>this is main branch</Text>
+      <Text>
+        app id: {Application.applicationName} - {Application.applicationId}
+      </Text>
+      <Text>update: {updateTime}</Text>
+      <Text>app version: {Application.nativeApplicationVersion}</Text>
+      <Text>build version: {Application.nativeBuildVersion}</Text>
       <StatusBar style="auto" />
     </View>
   )
