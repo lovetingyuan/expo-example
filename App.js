9244c59b401f71ebce8974090cbe86b3481ae9e1
胡浩 <huhao@seeyon.com>
2023-05-19 17:09:07 +0800

    chore

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import * as Updates from 'expo-updates'
-import RefreshWebView from './Web'
+// import RefreshWebView from './Web'
 
 function DetailsScreen() {
   const [isRefreshing, setRefreshing] = React.useState(false)
