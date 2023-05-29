e21c37aa7b2f443182e32aefd3409d321fa131da
胡浩 <huhao@seeyon.com>
2023-05-29 18:08:45 +0800

    chore

--- App.js
+++ App.js
@@ -15,6 +15,8 @@ function DetailsScreen() {
   )
 }
 
+import { useAppState } from '@react-native-community/hooks'
+
 function HomeScreen({ navigation }) {
   const [updateTime, setUpdateTime] = React.useState('')
   React.useEffect(() => {
@@ -24,6 +26,8 @@ function HomeScreen({ navigation }) {
   }, [])
   const color = useColorScheme()
   const color2 = useColorSchema2()
+  const currentAppState = useAppState()
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! this is change test github action</Text>
@@ -37,6 +41,9 @@ function HomeScreen({ navigation }) {
       <Text>
         color: {color} - {color2}
       </Text>
+      <Text>app state: {currentAppState}</Text>
+      <Text>timestamp: {Date.now()}</Text>
+
       <Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
       <Text>update channel: {Updates.channel}</Text>
       <Text>update created at: {Updates.createdAt.toString()}</Text>
